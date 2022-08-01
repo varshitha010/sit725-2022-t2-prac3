eb24dc83bc676af3f7de5ce33491d4646e3277c3
varshitha010 <varshu.kandimalla@gmail.com>
2022-08-01 19:39:42 +1000

    added template

---
+++ README.md
@@ -0,0 +1,9 @@
+- To Install npm dependencies
+```bash
+npm install
+```
+
+- To run the code
+```bash
+npm startcdcd
+```
---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+var port = process.env.port || 3000;
+
+app.listen(port, () => {
+  console.log("App listening to: " + port);
+});
